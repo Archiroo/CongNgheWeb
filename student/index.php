b0955ca4a27d821a948119f41d025ac3035b3ec3
TrHoangLong <longtrinh29102001@gmail.com>
2021-10-29 02:35:54 +0700

    giao dien sinh vien 3

--- student/index.php
+++ student/index.php
@@ -13,6 +13,38 @@ include('dashboard.php');
             </div>
         </div>
     </header>
+    <main>
+        <h2>Overview</h2>
+        <div class="dash-cards">
+            <div class="card-subject">
+                <div class="card-body">
+                    <img src="../image/cnweb.jpg" alt="">
+                    <h3>Công nghệ web</h3>
+                </div>
+                <div class="card-footer">
+                    <a href="">View all</a>
+                </div>
+            </div>
+            <div class="card-subject">
+                <div class="card-body">
+                    <img src="../image/database.png" alt="">
+                    <h3>Hệ quản trị CSDL</h3>
+                </div>
+                <div class="card-footer">
+                    <a href="">View all</a>
+                </div>
+            </div>
+            <div class="card-subject">
+                <div class="card-body">
+                    <img src="../image/ai.jpg" alt="">
+                    <h3>Trí tuệ nhân tạo</h3>
+                </div>
+                <div class="card-footer">
+                    <a href="">View all</a>
+                </div>
+            </div>
+        </div>
+    </main>
 </div>
 <?php
 include('footer.php');
