5efca225517a5017de3d438369e25a598a6d8e62
Archiroo <newbieit.nb@gmail.com>
2021-10-28 18:06:00 +0700

    side bar

---
+++ teacher/sidebar-teacher.php
@@ -0,0 +1,61 @@
+<?php
+include('header-teacher.php');
+?>
+<input type="checkbox" id="sidebar-toggle">
+<div class="sidebar">
+    <div class="sidebar-header">
+        <h3 class="brand">        
+            <span class="fas fa-user-graduate"></span>
+            <span>Education</span>
+        </h3>
+        <label for="sidebar-toggle" class="ti-menu-alt"></label>
+        <!--nút 3 ghạch-->
+    </div>
+    <div class="sidebar-menu">
+        <ul>
+            <li>
+                <a href="">
+                    <span class="ti-home"></span>
+                    <span>Home</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <span class="fas fa-scroll"></span>
+                    <span>Subject</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <span class="ti-clipboard"></span>
+                    <span>Homework</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <span class="far fa-user"></span>
+                    <span>Team</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <span class="ti-folder"></span>
+                    <span>Projects</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <span class="far fa-comments"></span>
+                    <span>Contact</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <span class="ti-settings"></span>
+                    <span>Acount</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+
