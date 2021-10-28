8b3790ebbc82d7971e508c4d1dff2e71f771f1e1
TrHoangLong <longtrinh29102001@gmail.com>
2021-10-29 00:39:42 +0700

    giao diện sinh viên 2

---
+++ student/index.php
@@ -0,0 +1,19 @@
+<?php
+include('dashboard.php');
+?>
+<div class="main-content">
+    <header>
+        <div class="search-wrapper">
+            <span class="ti-search"></span>
+            <input type="text" placeholder="Search">
+        </div>
+        <div class="social-icons">
+            <div>
+                <img src="../image/user.png" alt="" width="100px">
+            </div>
+        </div>
+    </header>
+</div>
+<?php
+include('footer.php');
+?>
