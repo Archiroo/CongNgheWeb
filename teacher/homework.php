3f57df7969126a3a4089dcbbac82f431bceb4d7c
Archiroo <newbieit.nb@gmail.com>
2021-10-29 00:37:35 +0700

    Xin là xin chúc mừng sinh nhật Long

---
+++ teacher/homework.php
@@ -0,0 +1,89 @@
+<?php
+include('sidebar.php');
+include('search.php');
+?>
+    <main>
+        <section class="recent">
+            <div class="activity-grid">
+                <div class="activity-card">
+                    <h3>Recent activity</h3>
+                    <div class="table-responsive">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>ID Teacher</th> <!--userlevel = 2-->
+                                    <th>ID Subject</th>
+                                    <th>Name</th>
+                                    <th>Start Date</th>
+                                    <th>End Date</th>
+                                    <th>Update</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>1</td>
+                                    <td>1</td>
+                                    <td>Thiết kế giao diện web</td>                                    
+                                    <td>29, Oc 2021</td>                                    
+                                    <td>29, Oc 2021</td>                                    
+                                    <td>
+                                        <a href="#">
+                                            Sửa
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <a href="#">
+                                            Xóa
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>1</td>
+                                    <td>1</td>
+                                    <td>Thiết kế giao diện web</td>                                    
+                                    <td>29, Oc 2021</td>                                    
+                                    <td>29, Oc 2021</td>                                    
+                                    <td>
+                                        <a href="#">
+                                            Sửa
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <a href="#">
+                                            Xóa
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>1</td>
+                                    <td>1</td>
+                                    <td>Thiết kế giao diện web</td>                                    
+                                    <td>29, Oc 2021</td>                                    
+                                    <td>29, Oc 2021</td>                                    
+                                    <td>
+                                        <a href="#">
+                                            Sửa
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <a href="#">
+                                            Xóa
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+<?php
+include('footer.php');
+?>
