6f70ee2b94c30f09c5823c45817282e59b10e33e
Archiroo <newbieit.nb@gmail.com>
2021-10-28 17:52:48 +0700

    chia folder, link

---
+++ teacher/footer-teacher.php
@@ -0,0 +1,7 @@
+    <!-- JQUERY -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- SCRIPT -->
+    <script src="js/teacher.js"></script>
+</body>
+
+</html>
