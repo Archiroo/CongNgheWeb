45e67db28a44d14e9e6a884634dbcff8f5ce8d65
Archiroo <newbieit.nb@gmail.com>
2021-10-30 13:29:36 +0700

    sửa file, đường dẫn

---
+++ teacher/connect_database/add_class.php
@@ -0,0 +1,8 @@
+<?php
+    include('sidebar_database.php');
+    include('search_database.php');
+?>
+
+<?php
+    include('footer_database.php');
+?>
---
+++ teacher/connect_database/search_database.php
@@ -0,0 +1,15 @@
+<?php
+include('header_database.php');
+?>
+<div class="main-content">
+    <header>
+        <div class="search-wrapper">
+            <span class="ti-search"></span>
+            <input type="text" placeholder="Search">
+        </div>
+        <div class="social-icons">
+            <div>
+                <img src="/image/user_1.jpg" alt="">
+            </div>
+        </div>
+    </header>
