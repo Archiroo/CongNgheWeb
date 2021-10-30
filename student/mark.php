bedec9ffae2c20cec3065065fd783027a9470338
TrHoangLong <longtrinh29102001@gmail.com>
2021-10-30 17:01:35 +0700

    giao dien mark

--- student/mark.php
+++ student/mark.php
@@ -1,3 +1,29 @@
 <?php
     include('./dashboard.php')
 ?>
+ <div class="main-mark">
+    <h1>Bài tập thiết kết giao diện</h1>
+
+    <table class="table-mark">
+        <tr>
+            <th>Tên sinh viên</th>
+            <td>Hồ Hồng Quân</td>
+        </tr>
+        <tr>
+            <th>Bài nộp</th>
+            <td>Hồ Hồng Quân</td>
+        </tr>
+        <tr>
+            <th>Trạng thái nộp bài</th>
+            <td>Hồ Hồng Quân</td>
+        </tr>
+        <tr>
+            <th>Ngày nộp</th>
+            <td>Hồ Hồng Quân</td>
+        </tr>
+        <tr>
+            <th>Điểm</th>
+            <td>Hồ Hồng Quân</td>
+        </tr>
+    </table>
+ </div>
