ce48d2ffc57a131ac3e8f3338be6260dc760d589
TrHoangLong <longtrinh29102001@gmail.com>
2021-10-30 14:58:36 +0700

    sửa lỗi subject

--- student/subject.php
+++ student/subject.php
@@ -4,16 +4,28 @@ include('dashboard.php');
 <div class="main-subject">
     <?php
     if (isset($_GET['id_subject'])) {
-        $id = $_GET['id_subject'];
-        $sql = "SELECT * FROM tb_subject WHERE id_subject=$id";
+        $id_subject = $_GET['id_subject'];
+        $sql = "SELECT * from tb_subject WHERE id_subject = '$id_subject'";
         $res = mysqli_query($conn, $sql);
+        if ($res == TRUE) {
+            $count = mysqli_num_rows($res);
+            if ($count == 1) {
+                $row = mysqli_fetch_assoc($res);
+                $name_subject = $row['name_subject'];
+            }
+        }
     }
     ?>
-
-    <h1 class="subject-name">subject name</h1>
+    <h1 class="subject-name"><?php echo $name_subject;?></h1>
+    
+    <?php
+        $sql2 
+    ?>
     <div class="homework-name">
         <div class="body">
-            <h2 id="title">title</h2>
+            <a href="">
+                <h2 id="title">title</h2>
+            </a>
             <div class="date">
                 <h3>Opened:</h3>
                 <h3>date</h3>
@@ -22,16 +34,13 @@ include('dashboard.php');
                 <h3>Due:</h3>
                 <h3>date</h3>
             </div>
-        </div>
-        <div class="edit">
-            <button><a href="">Add</a></button>
-            <button><a href="">Update</a></button>
-            <button><a href="">Delete</a></button>
         </div>
     </div>
     <div class="homework-name">
         <div class="body">
-            <h2 id="title">title</h2>
+            <a href="">
+                <h2 id="title">title</h2>
+            </a>
             <div class="date">
                 <h3>Opened:</h3>
                 <h3>date</h3>
@@ -40,11 +49,6 @@ include('dashboard.php');
                 <h3>Due:</h3>
                 <h3>date</h3>
             </div>
-        </div>
-        <div class="edit">
-            <button><a href="">Add</a></button>
-            <button><a href="">Update</a></button>
-            <button><a href="">Delete</a></button>
         </div>
     </div>
 </div>
